04b83f9ec3c3610810d58cfc204b27f66be1e5e4
raoud <raoud@INBOOK_X1_SLIM>
2022-11-15 16:27:05 +0530

    Get and update feature added.

--- EmpService/src/main/java/com/india/EmpService/Controller/EmpController.java
+++ EmpService/src/main/java/com/india/EmpService/Controller/EmpController.java
@@ -1,7 +1,11 @@
 package com.india.EmpService.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,4 +26,16 @@ public class EmpController {
 	public ServiceResponse<EmployeeResponseDTO> save(@RequestBody EmployeeRequestDTO employeeRequestDTO){
 		return empService.addNewEmployee(employeeRequestDTO);
 	}
+	
+	@GetMapping("/find/{name}/by/{sal}")
+	public ServiceResponse<EmployeeResponseDTO> save(@PathVariable String name,@PathVariable float sal){
+		return empService.findByNameAndSal(name, sal);
+	}
+	
+	@PutMapping("/update/{id}")
+	public void Update(@PathVariable int id,@RequestBody EmployeeRequestDTO employeeRequestDTO ) {
+		empService.updateEmpSal(id, employeeRequestDTO);
+	}
+	
+	
 }
--- EmpService/src/main/java/com/india/EmpService/Service/EmpService.java
+++ EmpService/src/main/java/com/india/EmpService/Service/EmpService.java
@@ -1,6 +1,10 @@
 package com.india.EmpService.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import com.india.EmpService.Dto.EmployeeRequestDTO;
@@ -19,4 +23,16 @@ public class EmpService {
 	public ServiceResponse<EmployeeResponseDTO> addNewEmployee(EmployeeRequestDTO employeeRequestDTO){
 		return restTemplate.postForObject(BASE_URL+"save", employeeRequestDTO, ServiceResponse.class);
 	}
+	
+	public ServiceResponse<EmployeeResponseDTO> findByNameAndSal(String name,float sal){
+		Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("name", name);
+        requestMap.put("sal", sal);
+		return restTemplate.getForObject(BASE_URL+"findByNameAndSal/{name}/By/{sal}",ServiceResponse.class,requestMap);
+	}
+	
+	public void updateEmpSal(int id,EmployeeRequestDTO empRequestDTO){
+		restTemplate.put(BASE_URL+"updateEmployee/"+id, empRequestDTO);;
+	}
+	
 }
